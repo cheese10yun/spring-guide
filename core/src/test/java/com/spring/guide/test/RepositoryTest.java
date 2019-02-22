15cf45a93f01faba32a70cdfa84a04e397dc7afd
Yun <cheese10yun@gmail.com>
2019-02-22 23:13:08 +0900

    CI 설정 변경

--- core/src/test/java/com/spring/guide/test/RepositoryTest.java
+++ core/src/test/java/com/spring/guide/test/RepositoryTest.java
@@ -3,7 +3,6 @@ package com.spring.guide.test;
 import com.spring.guide.test.config.TestProfile;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @ActiveProfiles(TestProfile.TEST)
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+//@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 @Ignore
 public class RepositoryTest {
 }
